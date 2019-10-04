07732816d6990c6548fbb8154569bbf57aa729bc
Kavleer <chriskabler@yahoo.com>
2019-10-04 11:44:59 -0400

    add css

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 // Your application is a note-taking app. It should display a list of all your notes and give you the ability to create new notes, edit old notes, and delete notes. Notes are made up of a title, text, optional tags, and the date/time most recently updated.
 
+// {
+// 	"username": "kc",
+// 	"password": "pass"
+// }
+
 listAllNotes
 
 newNote
@@ -14,12 +19,41 @@ note:
     tags:
 
 const app = {
-    {
-        data: {
-          credentials: {
-            username: sessionStorage.getItem('username'),
-            password: sessionStorage.getItem('password')
-          },
-          notes: []
-        },
+
+  data: {
+    credentials: {
+      username: sessionStorage.getItem('username'),
+      password: sessionStorage.getItem('password')
+    },
+    notes: []
+  },
+
+  setCredentials: function (username,password) {
+    this.data.credentials = {
+      username: username,
+      password: password
+    }
+    sessionStorage.setItem('username',username)
+    sessionStorage.setItem('username',username)
+  },
+
+
+  render: function () {
+    if (!this.data.credentials.username) || !this.data.credentials.password) {
+      showLoginForm()
+    }
+  }
 }
+
+function showLoginForm () {
+  document.getElementById('login-form')
+}
+
+function main(){
+
+  app.render()
+
+  const
+}
+
+main()
